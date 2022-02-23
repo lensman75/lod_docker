799dabb72309c647643922ae18f15e2745eb7f09
Alex <anministry.mgmt@gmail.com>
2022-02-23 21:28:41 +0200

    first commit

---
+++ Deploy_lod/copy_dump.sh
@@ -0,0 +1,2 @@
+scp -P2122 root@192.168.0.26:/var/lod_backup/2020-05-13.tar.bz2 /home/preston/Downloads/
+tar -xf /home/preston/Downloads/2020-05-13.tar.bz2
---
+++ Deploy_lod/update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+rm -rf lod_prj
+git clone git@bitbucket.org:lod_team/lod_prj.git
+rm -rf lod_prj/.git
